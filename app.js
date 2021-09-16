6bbe3507ac7ed530a79c172f31f8abbeef869856
Muhammad Ibraheem <ibrahimjhullan7@gmail.com>
2021-09-16 18:42:40 +0500

    Cookies in Express

--- app.js
+++ app.js
@@ -36,43 +36,39 @@ app.use(cookieParser('03133574584'));  // signing the cookie with a sceret key
 
 
 function auth(req, res, next) {
-  console.log(req.signedCookies);
 
   if (!req.signedCookies.user) {
-
     var authHeader = req.headers.authorization;
     if (!authHeader) {
       var err = new Error('You are not authenticated!');
       res.setHeader('WWW-Authenticate', 'Basic');
       err.status = 401;
-      return next(err);
-
+      next(err);
+      return;
     }
-
     var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
     var user = auth[0];
     var pass = auth[1];
     if (user == 'admin' && pass == 'password') {
-      res.cookie('user', 'admin', { signed: true })
+      res.cookie('user', 'admin', { signed: true });
       next(); // authorized
     } else {
-      err = new Error('You are not authenticated!');
+      var err = new Error('You are not authenticated!');
       res.setHeader('WWW-Authenticate', 'Basic');
       err.status = 401;
       next(err);
     }
-
-  } else {
+  }
+  else {
     if (req.signedCookies.user === 'admin') {
       next();
     }
     else {
-      err = new Error('You are not authenticated!');
+      var err = new Error('You are not authenticated!');
       err.status = 401;
-      return next(err);
+      next(err);
     }
   }
-
 }
 
 
